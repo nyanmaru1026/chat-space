8cfcbd012730f11ee65410e594afc0f6883705a7
nyanmaru1026 <nyanmaru2701@gmail.com>
2020-04-21 14:27:55 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="main-chat-message-list">
+      `<div class="main-chat-message-list" data-message-id=${message.id}>
          <div class="main-chat-message-list-info">
            <div class="main-chat-message-talker">
              ${message.user_name}
@@ -19,7 +19,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="main-chat-message-list">
+      `<div class="main-chat-message-list" data-message-id=${message.id}>
          <div class="main-chat-message-list-info">
            <div class="main-chat-message-talker">
              ${message.user_name}
@@ -39,6 +39,7 @@ $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
+ 
  $.ajax({
    url: url,
    type: "POST",
@@ -58,4 +59,29 @@ $('#new_message').on('submit', function(e){
   alert("メッセージ送信に失敗しました");
     });
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat-message-list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main-chat-message').append(insertHTML);
+      $('.main-chat-message').animate({ scrollTop: $('.main-chat-message')[0].scrollHeight});
+    }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
